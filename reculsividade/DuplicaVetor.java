d3b957addf9fcafa39d10112463c35847acd4c15
Reinan Gabriel <reinangabriel1520@gmail.com>
2020-03-05 16:51:04 -0300

    Create DuplicaVetor.java

---
+++ reculsividade/DuplicaVetor.java
@@ -0,0 +1,21 @@
+package reculsividade;
+
+public class DuplicaVetor {
+    public static void main(String[] args) {
+        Lista<Integer> vetor = new Lista<>();
+        vetor.add(5);
+        vetor.add(6);
+        vetor.add(7);
+        vetor.add(8);
+        vetor.add(9);
+        vetor.add(10);
+        vetor.add(11);
+        vetor.add(12);
+        vetor.add(13);
+
+        vetor.delete(8);
+
+        System.out.println(vetor.find(6));
+
+    }
+}
